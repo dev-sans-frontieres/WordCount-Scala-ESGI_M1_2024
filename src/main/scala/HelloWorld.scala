e6ff8806c12917cc15c7a8e3bf366eb31c743525
cceren <cceren@norsys.fr>
2024-04-04 15:14:42 +0200

    Ajout methode merge et map initial

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -2,6 +2,34 @@ package fr.esgi.funcProg
 
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, world ! This is my scala project!")
+    val text = """It is a period of civil wars in the galaxy. A brave alliance of
+      underground freedom fighters has challenged the tyranny and oppression of the awesome
+      GALACTIC EMPIRE.
+      Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships
+      have won their first victory in a battle with the powerful Imperial Starfleet.
+      The EMPIRE fears that another defeat could bring a thousand more solar systems into the
+      rebellion, and Imperial control over the galaxy would be lost forever.
+      To crush the rebellion once and for all, the EMPIRE is constructing a sinister new
+      battle station. Powerful enough to destroy an entire planet, its completion spells
+      certain doom for the champions of freedom."""
+
+    val mapping = text.split(" ").map(w => (w,1))
+
+    mapping.take(5).foreach(println)
+
+    def merge(m1: Map[String, Int], m2: Map[String, Int]): Map[String, Int] = {
+      val keys = m1.keySet ++ m2.keySet
+      val res = keys.map { k => 
+        val v1 = m1.getOrElse(k, 0)
+        val v2 = m2.getOrElse(k, 0)
+
+        (k, v1+v2)
+      }.toMap
+      res
+    }
+
+    val m1 = Map(("a" -> 1), ("b" -> 1))
+    val m2 = Map(("a"-> 1), ("c" -> 2))
+    println(merge(m1, m2))// Map(a -> 2, b -> 1, c -> 2))
   }
 }
